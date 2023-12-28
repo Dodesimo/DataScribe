6c9fe192d91dd3bf9dbe815491852a2b5a122f91
Devam Mondal <devammondal06@gmail.com>
2023-12-28 02:40:15 -0500

    added langchain helper class

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def main():
     return render_template('chat.html')
 
+@app.route("/get", methods=["GET", "POST"])
+def analytics():
+    query = request.form["query"]
+    return analytics(query)
 
 if __name__ == "__main__":
     app.run()
